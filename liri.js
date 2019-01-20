77c3f1a81725be1f40dfef63a0d3530fe904abe4
adamjorycromer <adamjorycromer@gmail.com>
2019-01-19 20:01:42 -0700

    ombd and bands in town work

--- liri.js
+++ liri.js
@@ -4,7 +4,7 @@ var keys = require("./keys.js");
 var fs = require("fs");
 var axios = require("axios");
 var moment = require("moment");
-var Spotify = require('node-spotify-api');
+var Spotify = require("node-spotify-api");
 
 //Variables equal to their keys hidden in the local .env file
 var omdbKey = keys.omdb.key;
@@ -14,13 +14,15 @@ var spotify = new Spotify(keys.spotify);
 // log user input
 var searchType = process.argv[2];
 var searchTerm = process.argv.slice(3).join(" ");
-var divider = ("\n\n------------------------\n\n")
+var divider = ("\n\n------------------------\n");
 
 var helpFunction = function () {
+
     console.log("Liri Bot is a tool which uses API Data from Spotify, Bands In Town, and Open Movie Database. \nIn the command line after typing 'node liri.js' type 'concert-this' followed by a band name to search for available concerts, \n'spotify-this-song' followed by a song name to see information about that song, \n'movie-this' and a movie title to see information about that movie, \nor 'do-what-it-says' for a surprise.");
 }
 
 var appendData = function (data) {
+
     fs.appendFile("log.txt", data, function (err) {
         if (err) {
             throw err;
@@ -28,8 +30,9 @@ var appendData = function (data) {
     });
 }
 
-var spotifyFunc = function () {
-    spotify.search({ type: 'track', query: searchTerm }, function (err, data) {
+var spotifyFunc = function (search) {
+
+    spotify.search({ type: 'track', query: search }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
@@ -42,7 +45,47 @@ var spotifyFunc = function () {
             "\nAlbum name: " + song.album.name +
             "\nOn Spotify: " + song.external_urls.spotify
         ].join("\n\n");
-        var cleanData = songInfo + divider;
+        var cleanData = "\n" + songInfo + divider;
+
+        appendData(cleanData);
+        console.log(cleanData);
+    });
+}
+
+var bandsFunc = function (search) {
+
+    url = "https://rest.bandsintown.com/artists/" + search + "/events?app_id=" + bandsKey;
+
+    axios.get(url).then(function (response) {
+
+        for (var i = 0; i < response.data.length; i++) {
+
+            var event = response.data[i];
+            var time = moment(event.datatime).format('LLLL');
+
+            var eventInfo = ["\nVenue: " + event.venue.name + "\nLocation: " + event.venue.city + ", " + event.venue.country + "\nTime: " + time
+            ].join("\n");
+
+            var cleanData = eventInfo + divider;
+
+            appendData(cleanData);
+            console.log(cleanData);
+        }
+    });
+}
+
+var omdbFunc = function (search) {
+    var url = "http://www.omdbapi.com/?apikey=" + omdbKey + "&t=" + search;
+
+    axios.get(url).then(function (response) {
+
+        var movie = response.data;
+        var date = moment(movie.Released).format('LL');
+
+        var movieInfo = ["\nTitle: " + movie.title + "\nDate of release: " + date + "\nIMDB Rating: " + movie.Ratings[0].value + "\nRotten Tomatoes Rating: " + movie.Ratings[1].value + "\nCountry of Origin: " + movie.Country + "\nLanguage: " + movie.Language + "\nPlot: " + movie.Plot + "\nDirector: " + movie.Director + "\nActors: " + movie.Actors
+        ].join("\n");
+
+        var cleanData = movieInfo + divider;
 
         appendData(cleanData);
         console.log(cleanData);
@@ -53,15 +96,18 @@ switch (searchType) {
 
     case "help":
         helpFunction();
+        break;
 
     case "concert-this":
+        bandsFunc(searchTerm);
         break;
 
     case "spotify-this-song":
-        spotifyFunc();
+        spotifyFunc(searchTerm);
         break;
 
     case "movie-this":
+        omdbFunc(searchTerm);
         break;
 
     case "do-what-it-says":
@@ -70,3 +116,4 @@ switch (searchType) {
     default:
         console.log("Welcome to Liri Bot.\nSearch help for instructions with what you can do with LIRI Bot.");
 }
+
